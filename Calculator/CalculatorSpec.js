5661cb892ec7499cb4290eba57ad87cb8e4d2bf2
Aaron Brooks <aaronbrooks322@gmail.com>
2014-11-06 03:32:51 -0600

    Swith versions of Jasmine to 1.3.1

--- Calculator/CalculatorSpec.js
+++ Calculator/CalculatorSpec.js
@@ -1,19 +1,31 @@
 // The describe block is required it takes a string and a function as arguments
 describe("Calculator", function() {
+    var calc;
+    
+    beforeEach(function() {
+        calc = new Calculator();
+
+        this.addMatchers({
+            toBeBetween: function(a,b) {
+              return this.actual >= a && this.actual <= b;
+            } 
+        });
+
+    });
 
     // The it block does all the heavy lifting and must be inside a describe block
     it("should be able to add 1 and 1", function() {
-        var calc = new Calculator();
         expect(calc.add(1,1)).toBe(2);
     });
 
     // You can have multiple if blocks in a describe
     it("should be able to divide 6 and 2", function() {
-        var calc = new Calculator();
         expect(calc.divide(6,2)).toBe(3);
     });
 
-    it
+    it("should be able to divide a rational number", function() {
+        expect(calc.divide(1,3)).toBeBetween(0.3, 0.34);
+    });
 });
 
 
